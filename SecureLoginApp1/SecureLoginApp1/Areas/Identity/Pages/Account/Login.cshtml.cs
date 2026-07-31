ad8caff7f7ed0aad24027a1cd49ef35d1cdd4feb
Bongani <tshabalalabongani315@gmail.com>
2026-07-31 15:28:56 +0200

    Update Login to record LastLogin and default redirect to Dashboard

--- SecureLoginApp1/SecureLoginApp1/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SecureLoginApp1/SecureLoginApp1/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SecureLoginApp1.Models;
+using SecureLoginApp1.Services;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
@@ -10,10 +11,17 @@ using System.Threading.Tasks;
 public class LoginModel : PageModel
 {
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IUserService _userService;
 
-    public LoginModel(SignInManager<ApplicationUser> signInManager)
+    /// <summary>
+    /// Initializes a new instance of the LoginModel class.
+    /// </summary>
+    /// <param name="signInManager">The SignInManager for authenticating users.</param>
+    /// <param name="userService">The UserService for updating user LastLogin.</param>
+    public LoginModel(SignInManager<ApplicationUser> signInManager, IUserService userService)
     {
         _signInManager = signInManager;
+        _userService = userService;
     }
 
     [BindProperty]
@@ -42,7 +50,8 @@ public class LoginModel : PageModel
 
     public async Task<IActionResult> OnPostAsync(string returnUrl = null)
     {
-        returnUrl ??= Url.Content("~/");
+        // Default redirect to Dashboard instead of home
+        returnUrl ??= Url.Content("~/Dashboard");
 
         if (!ModelState.IsValid)
         {
@@ -52,6 +61,13 @@ public class LoginModel : PageModel
         var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
         if (result.Succeeded)
         {
+            // Best-effort update of LastLogin; do not block sign-in if it fails
+            var user = await _userService.GetCurrentUserAsync(User);
+            if (user != null)
+            {
+                await _userService.UpdateLastLoginAsync(user);
+            }
+
             return LocalRedirect(returnUrl);
         }
 
